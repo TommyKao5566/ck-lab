eaf30032d5c4e5fa8b52606488832cd3dda71a1d
TommyKao5566 <tommy.kao@aceraeb.com>
2025-10-17 00:55:09 +0800

    [init]

---
+++ ckeditor/config.js
@@ -0,0 +1,131 @@
+﻿/**
+﻿ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+﻿ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+﻿ */
+﻿/*1110504  */
+﻿CKEDITOR.editorConfig = function( config ) {
+﻿	// Define changes to default configuration here.
+﻿	// For complete reference see:
+﻿	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+﻿	//1、設定使用語言
+﻿	  config.language = 'zh';
+﻿	 //2、設定ui顏色
+﻿	  config.uiColor = '#AADC6E';
+﻿	 //3、設定高度及寬度
+﻿	  config.height = 910;
+﻿	  config.width = 930;
+﻿	  config.fontSize_defaultLabel = '20px';
+﻿	  config.font_defaultLabel = '新細明體';
+﻿	  config.maxLines=5;
+﻿	  //還原次數
+﻿	  config.undoStackSize = 2;
+﻿	  //使用ENTER鍵產生的標籤
+﻿	  config.enterMode = CKEDITOR.ENTER_BR;
+﻿	  //使用Shift+ Enter組合鍵的行為。
+﻿	  config.shiftEnterMode = CKEDITOR.ENTER_BR;
+﻿	  //CKEDITOR.editor.filter 失去作用
+﻿	  config.allowedContent=true;
+﻿
+﻿
+﻿	  //4、設定功能鈕
+﻿    
+﻿  config.toolbarCanCollapse = false;
+﻿	// The toolbar groups arrangement, optimized for two toolbar rows.
+﻿		config.toolbar = [
+﻿		{ name: 'symbol', items : ['symbol_1','symbol_2','symbol_3','symbol_4','symbol_5','symbol_6','symbol_7'
+﻿			,'symbol_8','symbol_9','symbol_10','symbol_11','symbol_12','symbol_13','symbol_14']},
+﻿		//{ name: 'basicstyles', items: [ 'Bold', 'Italic','Underline','Subscript', 'Superscript']  },
+﻿		//{ name: 'basicstyles', items: [ 'Underline','Subscript', 'Superscript']  },
+﻿		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'Undo', 'Redo' ] },
+﻿		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent','Preview', 'Source'  ] }
+﻿		//,{ name: 'insert', items: [ 'Table'] }
+﻿		//,{ name: 'document', items: [ 'Preview', 'Source'] }		
+﻿		
+﻿	];
+﻿
+﻿	// Remove some buttons provided by the standard plugins, which are
+﻿	// not needed in the Standard(s) toolbar.
+﻿	config.removeButtons = '';
+﻿
+﻿	//config.extraPlugins = 'symbol_1,liststyle,tableresize,panel,button,richcombo,listblock,floatpanel,font,preview,notification,htmlwriter,undo,wordcount';
+﻿	config.extraPlugins = 'symbol_1,liststyle,tableresize,panel,button,richcombo,listblock,floatpanel,font,preview,wordcount';
+﻿
+﻿	// Set the most common block elements.
+﻿	config.format_tags = 'p;h1;h2;h3;pre';
+﻿
+﻿	// Simplify the dialog windows.
+﻿	config.removeDialogTabs = 'image:advanced;link:advanced';
+﻿	
+﻿	//編輯器內的右鍵菜單設為空。( table屬性會無法顯示)搜索關鍵字menuitem，查看源碼
+﻿	//CKEDITOR.config.menu_groups = '' ;
+﻿	
+﻿	//設置快捷鍵
+﻿    config.keystrokes = [ 
+﻿       [CKEDITOR.ALT + 121 /*F10*/,'toolbarFocus'],//獲取焦點
+﻿       [CKEDITOR.ALT + 122 /*F11*/,'elementsPathFocus'],//元素焦點
+﻿       [CKEDITOR.SHIFT + 121 /*F10*/,'contextMenu'],//文本菜單
+﻿       [CKEDITOR.CTRL + 90 /*Z*/,'撤消'],//撤銷
+﻿       [CKEDITOR.CTRL + 89 /*Y*/,'redo'],//重做
+﻿       [CKEDITOR.CTRL + CKEDITOR.SHIFT + 90 /*Z*/,'redo'],//
+﻿       //[CKEDITOR.CTRL + 76 /*L*/,'link'],//鏈接
+﻿       [CKEDITOR.CTRL + 66 /* B */,'bold'],//粗體
+﻿       [CKEDITOR.CTRL + 73 /*I*/,'italic'],//斜體
+﻿       [CKEDITOR.CTRL + 85 /*U*/,'underline'],//下劃線
+﻿       [CKEDITOR.ALT + 109 /*-*/,'toolbarCollapse'] 
+﻿    ];
+﻿
+﻿    //設置快捷鍵可能與瀏覽器快捷鍵衝突插件/ keystrokes / plugin.js。
+﻿    config.blockedKeystrokes = [ 
+﻿        CKEDITOR.CTRL + 66 /*B*/
+﻿        ,CKEDITOR.CTRL + 73 /*I*/
+﻿        ,CKEDITOR.CTRL + 85 /*U*/ 
+﻿		,CKEDITOR.CTRL + 79 /*O 開啟舊檔*/ 
+﻿		,CKEDITOR.CTRL + 83 /*S 儲存*/
+﻿		,CKEDITOR.CTRL + 82 /*R */
+﻿		,CKEDITOR.CTRL + 87 /*W ?*/ 
+﻿		,CKEDITOR.CTRL + 75 /*K 連結?*/
+﻿		
+﻿    ];
+﻿    
+﻿ // 字型選單 (名稱/字體)
+﻿    CKEDITOR.config.font_names = 'Arial/Arial, Helvetica, sans-serif;' +
+﻿    'Comic Sans MS/Comic Sans MS, cursive;' +
+﻿    'Courier New/Courier New, Courier, monospace;' +
+﻿    'Georgia/Georgia, serif;' +
+﻿    'Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;' +
+﻿    'Tahoma/Tahoma, Geneva, sans-serif;' +
+﻿    'Times New Roman/Times New Roman, Times, serif;' +
+﻿    'Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;' +
+﻿    'Verdana/Verdana, Geneva, sans-serif;新細明體;標楷體;微軟正黑體';
+﻿	
+﻿    
+﻿    config.wordcount = {
+﻿
+﻿    		// Whether or not you want to show the Paragraphs Count
+﻿    		showParagraphs: true,
+﻿    		// Whether or not you want to show the Word Count
+﻿    		showWordCount: true,
+﻿    		// Whether or not you want to show the Char Count
+﻿    		showCharCount: false,
+﻿    		// Whether or not you want to count Spaces as Chars
+﻿    		countSpacesAsChars: false,
+﻿    		// Whether or not to include Html chars in the Char Count
+﻿    		countHTML: false,		
+﻿    		// Maximum allowed Word Count, -1 is default for unlimited
+﻿    		maxWordCount: -1,
+﻿    		// Maximum allowed Char Count, -1 is default for unlimited
+﻿    		maxCharCount: -1,
+﻿			// 設定最大段落數
+﻿			maxParagraphs: 10 ,
+﻿    		// Add filter to add or remove element before counting (see CKEDITOR.htmlParser.filter), Default value : null (no filter)
+﻿    		filter: new CKEDITOR.htmlParser.filter({
+﻿    			elements: {
+﻿    				div: function( element ) {
+﻿    					if(element.attributes.class == 'mediaembed') {
+﻿    						return false;
+﻿    					}
+﻿    				}
+﻿    			}
+﻿    		})
+﻿    	};	
+﻿};
---
+++ ckeditor/plugins/symbol_1/plugin.js
@@ -0,0 +1,182 @@
+//===plugins/symbol_1/plugn.js==========
+  
+(function()
+{
+	// The pastetext command definition.
+	var symbolCmd_1 =
+	{exec : function( editor ){editor.insertHtml('，');return true;}};
+	
+	var symbolCmd_2 =
+	{exec : function( editor ){editor.insertHtml('。');return true;}};
+	
+	var symbolCmd_3 =
+	{exec : function( editor ){editor.insertHtml('、');return true;}};
+	
+	var symbolCmd_4 =
+	{exec : function( editor ){editor.insertHtml('；');return true;}};
+	
+	var symbolCmd_5 =
+	{exec : function( editor ){editor.insertHtml('：');return true;}};
+	
+	var symbolCmd_6 =
+	{exec : function( editor ){editor.insertHtml('！');return true;}};
+	
+	var symbolCmd_7 =
+	{exec : function( editor ){editor.insertHtml('？');return true;}};
+	
+	var symbolCmd_8 =
+	{exec : function( editor ){editor.insertHtml('V');return true;}};
+	
+	var symbolCmd_9 =
+	{exec : function( editor ){editor.insertHtml('—');return true;}};
+	
+	var symbolCmd_10 =
+	{exec : function( editor ){editor.insertHtml('〇');return true;}};
+	
+	var symbolCmd_11 =
+	{exec : function( editor ){editor.insertHtml('※');return true;}};
+	
+	var symbolCmd_12 =
+	{exec : function( editor ){editor.insertHtml('（）');return true;}};
+	
+	var symbolCmd_13 =
+	{exec : function( editor ){editor.insertHtml('「」');return true;}};
+	
+	var symbolCmd_14 =
+	{exec : function( editor ){editor.insertHtml('『』');return true;}};
+  
+  
+	// Register the plugin.
+	CKEDITOR.plugins.add( 'symbol_1', 
+	{
+		init : function( editor )
+		{
+			var commandName_1 = 'symbol_1'; 
+			editor.addCommand( commandName_1, symbolCmd_1 );
+			editor.ui.addButton( commandName_1,
+			{
+			label : '逗號', 
+			command : commandName_1,
+			icon : this.path + 'images/mark01.gif'
+			});
+			
+			var commandName_2 = 'symbol_2'; 
+			editor.addCommand( commandName_2, symbolCmd_2 );
+			editor.ui.addButton( commandName_2,
+			{
+			label : '句號', 
+			command : commandName_2,
+			icon : this.path + 'images/mark02.gif'
+			});
+			
+			var commandName_3 = 'symbol_3'; 
+			editor.addCommand( commandName_3, symbolCmd_3 );
+			editor.ui.addButton( commandName_3,
+			{
+			label : '頓號', 
+			command : commandName_3,
+			icon : this.path + 'images/mark03.gif'
+			});
+			
+			var commandName_4 = 'symbol_4'; 
+			editor.addCommand( commandName_4, symbolCmd_4 );
+			editor.ui.addButton( commandName_4,
+			{
+			label : '分號', 
+			command : commandName_4,
+			icon : this.path + 'images/mark04.gif'
+			});
+			
+			var commandName_5 = 'symbol_5'; 
+			editor.addCommand( commandName_5, symbolCmd_5 );
+			editor.ui.addButton( commandName_5,
+			{
+			label : '冒號', 
+			command : commandName_5,
+			icon : this.path + 'images/mark05.gif'
+			});
+			
+			var commandName_6 = 'symbol_6'; 
+			editor.addCommand( commandName_6, symbolCmd_6 );
+			editor.ui.addButton( commandName_6,
+			{
+			label : '驚嘆號', 
+			command : commandName_6,
+			icon : this.path + 'images/mark06.gif'
+			});
+			
+			var commandName_7 = 'symbol_7'; 
+			editor.addCommand( commandName_7, symbolCmd_7 );
+			editor.ui.addButton( commandName_7,
+			{
+			label : '問號', 
+			command : commandName_7,
+			icon : this.path + 'images/mark07.gif'
+			});
+			
+			var commandName_8 = 'symbol_8'; 
+			editor.addCommand( commandName_8, symbolCmd_8 );
+			editor.ui.addButton( commandName_8,
+			{
+			label : 'V', 
+			command : commandName_8,
+			icon : this.path + 'images/mark08.gif'
+			});
+			
+			var commandName_9 = 'symbol_9'; 
+			editor.addCommand( commandName_9, symbolCmd_9 );
+			editor.ui.addButton( commandName_9,
+			{
+			label : '連接號', 
+			command : commandName_9,
+			icon : this.path + 'images/mark09.gif'
+			});
+			
+			var commandName_10 = 'symbol_10'; 
+			editor.addCommand( commandName_10, symbolCmd_10 );
+			editor.ui.addButton( commandName_10,
+			{
+			label : '〇', 
+			command : commandName_10,
+			icon : this.path + 'images/mark10.gif'
+			});
+			
+			var commandName_11 = 'symbol_11'; 
+			editor.addCommand( commandName_11, symbolCmd_11 );
+			editor.ui.addButton( commandName_11,
+			{
+			label : '參考標記', 
+			command : commandName_11,
+			icon : this.path + 'images/mark11.gif'
+			});
+			
+			var commandName_12 = 'symbol_12'; 
+			editor.addCommand( commandName_12, symbolCmd_12 );
+			editor.ui.addButton( commandName_12,
+			{
+			label : '括號', 
+			command : commandName_12,
+			icon : this.path + 'images/mark12.gif'
+			});
+			
+			var commandName_13 = 'symbol_13'; 
+			editor.addCommand( commandName_13, symbolCmd_13 );
+			editor.ui.addButton( commandName_13,
+			{
+			label : '引號', 
+			command : commandName_13,
+			icon : this.path + 'images/mark13.gif'
+			});
+			
+			var commandName_14 = 'symbol_14'; 
+			editor.addCommand( commandName_14, symbolCmd_14 );
+			editor.ui.addButton( commandName_14,
+			{
+			label : '雙引號', 
+			command : commandName_14,
+			icon : this.path + 'images/mark14.gif'
+			});
+
+		},
+	});
+})();
